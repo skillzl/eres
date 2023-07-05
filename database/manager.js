6411a160c4d6610688cd2d35a00df943f7902af3
Catalin Ta <user@skillzl.me>
2023-07-06 01:11:32 +0300

    🥬 mongo: schemas manager format

--- database/manager.js
+++ database/manager.js
@@ -42,58 +42,42 @@ module.exports = class Manager {
 		return result;
 	}
 
-	async getUserById(id) {
+	static async getUserById(id) {
 		let user = await userModel.findOne({ userId: id });
-		if (!user) {
-		  user = await this.createUser(id);
-		}
-	
-		return {
-		  user
-		};
-	  }
+		if (!user) { user = await this.createUser(id); }
+		return { user };
+	}
 
-	  async updateUserById(id, data) {
-		if (typeof data !== 'object') {
-		  throw Error("'data' must be an object");
-		}
-	
+	static async updateUserById(id, data) {
+		if (typeof data !== 'object') { throw Error('\'data\' must be an object'); }
 		const user = await this.getUserById(id);
-		if (!user) {
-		  await this.addUser(id);
-		}
-	
-		await userModel.findOneAndUpdate({ userId: id }, data);
-	  }
-	
-	  async removeUser(id) {
-		await userModel.findOneAndDelete({ userId: id });
-	  }
+		if (!user) { await this.addUser(id); }
 
-	  async getGuildById(id) {
+		await userModel.findOneAndUpdate({ userId: id }, data);
+	}
+
+	static async removeUser(id) {
+		await userModel.findOneAndDelete({ userId: id });
+	}
+
+	static async getGuildById(id) {
 		let guild = await serverModel.findOne({ guildId: id });
-	
-		if (!guild) {
-		  guild = await this.addGuild(id);
-		}
+
+		if (!guild) { guild = await this.addGuild(id); }
 		return guild;
-	  }
-	
-	  async updateGuildById(id, settings) {
-		if (typeof settings !== 'object') {
-		  throw Error("'settings' must be an object");
-		}
-	
+	}
+
+	static async updateGuildById(id, settings) {
+		if (typeof settings !== 'object') { throw Error('\'settings\' must be an object'); }
+
 		const guild = await this.getGuildById(id);
-	
-		if (!guild) {
-		  await this.addGuild(id);
-		}
-	
+
+		if (!guild) { await this.addGuild(id); }
+
 		await serverModel.findOneAndUpdate({ guildId: id }, settings);
-	  }
-	
-	  async removeGuild(id) {
+	}
+
+	static async removeGuild(id) {
 		await serverModel.findOneAndDelete({ guildId: id });
-	  }
+	}
 };
