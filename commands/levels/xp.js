ba29310a08c73992d96b5a5d6d193c1f525bc019
Catalin Ta <user@skillzl.me>
2023-07-10 21:03:21 +0300

    🌟 levels: xp fetch command

---
+++ commands/levels/xp.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/CommandClass');
+const db = require('../../database/manager');
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = class Rank extends Command {
+	constructor(client) {
+		super(client, {
+			data: new SlashCommandBuilder()
+				.setName('xp')
+				.setDescription('Fetch the current xp points a user have.')
+				.addUserOption(option => option.setName('target').setDescription('The user')
+					.setRequired(false))
+				.setDMPermission(false),
+			usage: 'xp',
+			category: 'Levels',
+			permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
+		});
+	}
+	async run(client, interaction) {
+		const member = interaction.options.getUser('target') || interaction.user;
+		const { user } = await db.getUserById(member.id);
+
+		interaction.reply(`<:star_emoji:1126279940321574913> ${member.username} has earned **${user.xp.toLocaleString()}** xp points.`);
+	}
+};
