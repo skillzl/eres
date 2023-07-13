a5d8c1ac1c8f4bac70ab989a6133f94e9546c4f9
Catalin Ta <user@skillzl.me>
2023-07-14 01:06:59 +0300

    📌 chore: fix & cat originity

--- commands/profile/profile.js
+++ commands/profile/profile.js
@@ -1,26 +1,24 @@
 const Command = require('../../structures/CommandClass');
-const db = require('../../database/manager');
 
 const { SlashCommandBuilder } = require('discord.js');
 
-module.exports = class Xp extends Command {
+module.exports = class Profile extends Command {
 	constructor(client) {
 		super(client, {
 			data: new SlashCommandBuilder()
-				.setName('xp')
-				.setDescription('Fetch the current xp points an user have.')
+				.setName('profile')
+				.setDescription('Fetch the current profile stats an user have.')
 				.addUserOption(option => option.setName('target').setDescription('The user')
 					.setRequired(false))
 				.setDMPermission(false),
-			usage: 'xp',
-			category: 'Levels',
+			usage: 'profile',
+			category: 'Profile',
 			permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
 		});
 	}
 	async run(client, interaction) {
 		const member = interaction.options.getUser('target') || interaction.user;
-		const { user } = await db.getUserById(member.id);
 
-		interaction.reply(`<:star_emoji:1126279940321574913> ${member.username} has earned **${user.xp.toLocaleString()}** xp points.`);
+		interaction.reply('wip ' + member.username);
 	}
 };
