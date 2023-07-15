c3d43f0f18547209f431452acf3003482075213d
Catalin Ta <user@skillzl.me>
2023-07-16 00:59:25 +0300

    🪙 balance: command

---
+++ commands/economy/balance.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/CommandClass');
+const db = require('../../database/manager');
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = class Balance extends Command {
+	constructor(client) {
+		super(client, {
+			data: new SlashCommandBuilder()
+				.setName('balance')
+				.setDescription('Customize your profile, by setting a new about me text')
+				.addUserOption(option => option.setName('target').setDescription('The user')
+					.setRequired(false))
+				.setDMPermission(false),
+			usage: 'balance',
+			category: 'Economy',
+			permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
+		});
+	}
+	async run(client, interaction) {
+		const member = interaction.options.getUser('target') || interaction.user;
+		const { user } = await db.getUserById(member.id);
+
+		interaction.reply(`<:balance_emoji:1129875960188112966> ${member.username} has **${user.balance.toLocaleString()}** coins in total.`);
+	}
+};
