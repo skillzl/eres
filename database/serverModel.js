308f80ffd06c063a2b2e0ea0c3daa04bab93d38b
Catalin Ta <user@skillzl.me>
2023-07-16 22:42:50 +0300

    🏬 database: modal udpate

--- database/serverModel.js
+++ database/serverModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const serverSchema = new mongoose.Schema({
-	serverId: { type:String }, prefix: { type:String },
+	serverId: { type:String },
+	prefix: { type:String },
 });
 
 module.exports = mongoose.model('server', serverSchema);
