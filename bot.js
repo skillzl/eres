8634bf8d22b79f4095f6d1721fee6bbfbfad8039
Mona Lisa <jwstephenson1000@gmail.com>
2023-12-20 13:31:59 -0600

    whoops made a name mistake

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const Client = require('./structures/Client');
 const fs = require('fs');
 const client = new Client();
-const anotherFileContent = fs.readFileSync('./deploy.js', 'utf8');
+const deploy = fs.readFileSync('./deploy.js', 'utf8');
 
 // Log into the bot
 client.login();
 
 // Run the deploy file to deploy commands
-eval(anotherFileContent);
+eval(deploy);
 
 // error
 process.on('uncaughtException', err => console.error(err.stack));
