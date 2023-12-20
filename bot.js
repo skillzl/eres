b48832324c55d4a5457c695840f92af82619a9c9
Mona Lisa <jwstephenson1000@gmail.com>
2023-12-20 13:31:18 -0600

    added some notes and made bot.js run deploy everytime bot is started (Usefull for when you have to add new commands)

--- bot.js
+++ bot.js
@@ -1,8 +1,16 @@
+// Import stuff
 require('dotenv').config();
 const Client = require('./structures/Client');
-
+const fs = require('fs');
 const client = new Client();
+const anotherFileContent = fs.readFileSync('./deploy.js', 'utf8');
+
+// Log into the bot
 client.login();
 
+// Run the deploy file to deploy commands
+eval(anotherFileContent);
+
+// error
 process.on('uncaughtException', err => console.error(err.stack));
 process.on('unhandledRejection', err => console.error(err.stack));
