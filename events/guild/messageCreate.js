ea0e9b2fc6ea8de6e5a287cb079263a1da6e1a4d
Catalin Ta <user@skillzl.me>
2023-12-22 19:18:17 +0200

    🐛 bug: returned to old level up message (xp would not add)

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,50 +1,35 @@
+/* eslint-disable no-shadow */
 const Event = require('../../structures/EventClass');
 const db = require('../../database/manager');
-const { MessageAttachment, MessageMentions, AttachmentBuilder } = require('discord.js');
-const { createCanvas, loadImage } = require('canvas');
-const path = require('path');
 
 module.exports = class MessageCreate extends Event {
-  constructor(client) {
-    super(client, {
-      name: 'messageCreate',
-      category: 'message',
-    });
-  }
+	constructor(client) {
+		super(client, {
+			name: 'messageCreate',
+			category: 'message',
+		});
+	}
 
-  async run(message) {
-    if (!message.author.bot) {
-      const { user } = await db.getUserById(message.author.id);
-      const xp = Math.ceil(Math.random() * (1 * 5));
+	async run(message) {
+		if (!message.author.bot) {
+			const { user } = await db.getUserById(message.author.id);
+			const xp = Math.ceil(Math.random() * (1 * 5));
 
-      const calculateUserXp = (xp) => Math.floor(0.1 * Math.sqrt(xp));
+			const calculateUserXp = (xp) => Math.floor(0.1 * Math.sqrt(xp));
 
-      const oldLevel = calculateUserXp(user.xp || 0);
-      const newLevel = calculateUserXp((user.xp || 0) + xp);
+			const level = calculateUserXp(user.xp);
+			const newLevel = calculateUserXp(user.xp + xp);
 
-      if (newLevel > oldLevel) {
-        const canvas = createCanvas(1026, 285);
-        const ctx = canvas.getContext('2d');
-        
-        const background = await loadImage(path.join('./assets/canva/level-up-background.png'));
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+			if (newLevel > level) {
+				const msg = await message.reply(`<:star_emoji:1126279940321574913> Congratulations, you leveled up to level \`${newLevel}\`!`);
+				setTimeout(() => {
+					msg?.delete();
+				}, 10000);
+			}
 
-        ctx.font = '30px sans-serif';
-        ctx.fillStyle = 'white';
-        ctx.textAlign = 'center';
-        ctx.fillText(`Congratulations, ${message.author.username}!`, canvas.width / 2, canvas.height / 2);
-        ctx.fillText(`Level Up: ${oldLevel} --> ${newLevel}`, canvas.width / 2, canvas.height / 2 + 40);
-
-        const buffer = canvas.toBuffer(); 
-        const attachment = new AttachmentBuilder(buffer, 'levelup.png');
-
-        await message.channel.send({
-          content: `Congratulations, ${message.author.username}! \n\nLevel Up: ${oldLevel} --> ${newLevel}`,
-          files: [attachment],
-        });
-
-        await db.updateUserById(message.author.id, { xp: user.xp + xp });
-      }
-    }
-  }
+			await db.updateUserById(message.author.id, {
+				xp: user.xp + xp,
+			});
+		}
+	}
 };
