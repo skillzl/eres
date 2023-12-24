97ced96a012ee965460f08839334ee9b3a820552
Catalin Ta <user@skillzl.me>
2023-12-24 14:18:02 +0200

    ⬆️ process: events & player events handler

---
+++ functions/Player.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const { Player } = require('discord-player');
+
+module.exports = (client) => {
+	client.Player = async () => {
+		const playerEventsPath = path.join(__dirname, '../events/player');
+		const playerEventFiles = fs.readdirSync(playerEventsPath).filter((file) => file.endsWith('.js'));
+
+		const player = Player.singleton();
+
+		for (const file of playerEventFiles) {
+			const filePath = path.join(playerEventsPath, file);
+			const event = require(filePath);
+			player.events.on(event.name, (...args) => event.execute(...args, client));
+		}
+	};
+};
