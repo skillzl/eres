1ac4dd0af1ccef771abd0467be4f617486e99c10
Catalin Ta <admin@skillzl.dev>
2024-01-19 23:53:31 +0200

    🐛 bug: fixed skip misscount

--- commands/player/skip.js
+++ commands/player/skip.js
@@ -1,9 +1,11 @@
 const Command = require('../../structures/CommandClass');
 const { SlashCommandBuilder } = require('discord.js');
+const { Collection } = require('discord.js');
 
+const db = require('../../database/manager');
 const { Player } = require('discord-player');
 
-const skipVotes = new Map();
+const skipVotes = new Collection();
 
 module.exports = class Skip extends Command {
 	constructor(client) {
@@ -24,36 +26,46 @@ module.exports = class Skip extends Command {
  * @param {Interaction} interaction - The interaction object representing the command interaction.
  */
 	async run(client, interaction) {
+		// Get the player singleton instance and the queue for the current guild
 		const player = Player.singleton();
 		const queue = player.nodes.get(interaction.guild.id);
+		const database = await db.findServer(interaction.guild.id);
 
 		// Check if there is any music currently playing
 		if (!queue || !queue.isPlaying()) {
 			return await interaction.reply(`${client.emoji.red_emoji} There isn't currently any music playing.`);
 		}
 
-		const usersInChannel = interaction.member.voice.channel.members.size;
+		// Get the number of users in the voice channel
+		const usersInChannel = interaction.member.voice.channel.members.filter(member => !member.user.bot).size;
+
+		// Get the DJ role from the database
+		const djRole = database?.djrole;
 
 		// Initialize the skipVotes set if it doesn't exist
-		if (!skipVotes[interaction.guild.id]) {
-			skipVotes[interaction.guild.id] = new Set();
+		if (!skipVotes.has(interaction.guild.id)) {
+			skipVotes.set(interaction.guild.id, new Set());
 		}
 
 		// Add the user's vote to skip the track
-		const skipVotesTotal = skipVotes[interaction.guild.id];
+		const skipVotesTotal = skipVotes.get(interaction.guild.id);
 		skipVotesTotal.add(interaction.user.id);
 
 		// Check if the number of skip votes reaches the required threshold
-		if (skipVotesTotal.size >= Math.ceil(usersInChannel * 0.35)) {
+		if (skipVotesTotal.size >= usersInChannel / 2) {
 			// Skip the current track and reset the skipVotes set
 			queue.node.skip();
-			skipVotes[interaction.guild.id] = new Set();
+			skipVotes.delete(interaction.guild.id);
+			return await interaction.reply(`${client.emoji.green_emoji} The track **${queue.currentTrack.title}** was skipped.`);
+		}
+		else if (interaction.member.roles.cache.has(djRole)) {
+			queue.node.skip();
 			return await interaction.reply(`${client.emoji.green_emoji} The track **${queue.currentTrack.title}** was skipped.`);
 		}
 		else {
 			// Calculate the number of votes needed to skip the track
-			const votesNeeded = Math.ceil(usersInChannel * 0.35) - skipVotesTotal.size - 1;
-			return await interaction.reply(`You voted to skip. Required ${votesNeeded} more vote(s) needed to skip the track.`);
+			const votesNeeded = usersInChannel / 2;
+			return await interaction.reply(`You voted to skip. Required ${Math.round(votesNeeded - 1)} more vote(s) needed to skip the track.`);
 		}
 	}
 };
--- database/serverModel.js
+++ database/serverModel.js
@@ -4,6 +4,7 @@ const serverSchema = new mongoose.Schema({
 	serverId: { type: String },
 	prefix: { type: String },
 	autorole: { type: String, default: null },
+	djrole : { type: String, default: null },
 	welcome: { type: String, default: null },
 	leave: { type: String, default: null },
 	i18n: { type: String, default: 'en', required: true },
